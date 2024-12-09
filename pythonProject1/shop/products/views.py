56ae5f45bfaf6c08912633ffc2b07307e21b59fb
g0r81 <dagiojanadze@gmail.com>
2024-12-09 19:55:09 +0400

    added product

--- pythonProject1/shop/products/views.py
+++ pythonProject1/shop/products/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {"products": products})
+
+
+def product_detail(request):
+    return render(request, 'product_detail.html')
